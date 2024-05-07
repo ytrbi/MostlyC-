eb76418c4bb5aed50dd523eacb8cbc957eee2c48
𝓜𝓪𝓵𝔂 <97814232+15n@users.noreply.github.com>
2024-05-07 12:21:34 +0300

    Create practice.cs

---
+++ practice.cs
@@ -0,0 +1,28 @@
+// getting input
+      Console.WriteLine("How old are you?");
+      string input = Console.ReadLine();
+      Console.WriteLine($"You are {input} years old!");
+
+// ASCII Art
+
+            // this ghost called: WALLDY
+            Console.WriteLine(" .-."); 
+            Console.WriteLine("(o o)");
+            Console.WriteLine("| O |");
+            Console.WriteLine("|   |");
+            Console.WriteLine("'~~~'");
+            // --------------------------
+            Console.WriteLine("c_     ___");
+            Console.WriteLine(" \\__|__|__");
+            Console.WriteLine("  \\( o_o)");
+            Console.WriteLine("   > ~  >");
+            Console.WriteLine("  /  / \\");
+            Console.WriteLine(" /  /   \\");
+            Console.WriteLine(" \\  )   c/");
+            Console.WriteLine("  /  /");
+            Console.WriteLine(" /  /|");
+            Console.WriteLine("(  ( \\");
+            Console.WriteLine("|  |  \\");
+            Console.WriteLine("| | ) |");
+            Console.WriteLine("(_ /");
+
